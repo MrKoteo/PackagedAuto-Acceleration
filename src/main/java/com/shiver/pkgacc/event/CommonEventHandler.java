a02202cbe20cf2ef0f1367e64968e57ce6bd69c1
Shiver211 <2863677073@qq.com>
2026-06-28 14:56:26 +0800

    UPDATE

--- src/main/java/com/shiver/pkgacc/event/CommonEventHandler.java
+++ src/main/java/com/shiver/pkgacc/event/CommonEventHandler.java
@@ -3,10 +3,12 @@ package com.shiver.pkgacc.event;
 import com.shiver.pkgacc.PackagedAcceleration;
 import com.shiver.pkgacc.item.ItemSpeedCard;
 import com.shiver.pkgacc.speed.SpeedCardHelper;
+import net.minecraft.entity.item.EntityItem;
 import net.minecraft.entity.player.EntityPlayer;
 import net.minecraft.item.ItemStack;
 import net.minecraft.tileentity.TileEntity;
 import net.minecraftforge.common.MinecraftForge;
+import net.minecraftforge.event.world.BlockEvent;
 import net.minecraftforge.event.entity.player.PlayerInteractEvent;
 import net.minecraftforge.fml.common.eventhandler.EventPriority;
 import net.minecraftforge.fml.common.eventhandler.SubscribeEvent;
@@ -36,6 +38,24 @@ public class CommonEventHandler {
         event.setCanceled(true);
         if(!event.getWorld().isRemote) {
             player.openGui(PackagedAcceleration.instance, GUI_SPEED_CARD, event.getWorld(), event.getPos().getX(), event.getPos().getY(), event.getPos().getZ());
+        }
+    }
+
+    @SubscribeEvent(priority = EventPriority.LOWEST)
+    public void onBlockBreak(BlockEvent.BreakEvent event) {
+        if(event.getWorld().isRemote) {
+            return;
+        }
+        TileEntity tile = event.getWorld().getTileEntity(event.getPos());
+        int cards = SpeedCardHelper.getCards(tile);
+        if(cards > 0) {
+            event.getWorld().spawnEntity(new EntityItem(
+                    event.getWorld(),
+                    event.getPos().getX()+0.5D,
+                    event.getPos().getY()+0.5D,
+                    event.getPos().getZ()+0.5D,
+                    new ItemStack(ItemSpeedCard.INSTANCE, cards)));
+            SpeedCardHelper.setCards(tile, 0);
         }
     }
 }
---
+++ src/main/java/com/shiver/pkgacc/mixin/MixinTileUpdate.java
@@ -0,0 +1,88 @@
+package com.shiver.pkgacc.mixin;
+
+import com.shiver.pkgacc.speed.SpeedCardHelper;
+import net.minecraft.util.ITickable;
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.Pseudo;
+import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.Inject;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
+import thelm.packagedauto.tile.TileBase;
+
+import java.util.Map;
+import java.util.WeakHashMap;
+
+@Pseudo
+@Mixin(targets = {
+        "thelm.packagedauto.tile.TileCrafter",
+        "thelm.packagedauto.tile.TilePackager",
+        "thelm.packagedauto.tile.TilePackagerExtension",
+        "thelm.packagedavaritia.tile.TileExtremeCrafter",
+        "thelm.packagedexcrafting.tile.TileBasicCrafter",
+        "thelm.packagedexcrafting.tile.TileAdvancedCrafter",
+        "thelm.packagedexcrafting.tile.TileEliteCrafter",
+        "thelm.packagedexcrafting.tile.TileUltimateCrafter",
+        "thelm.packagedexcrafting.tile.TileCombinationCrafter",
+        "thelm.packagedexcrafting.tile.TileEnderCrafter",
+        "thelm.packagedastral.tile.TileDiscoveryCrafter",
+        "thelm.packagedastral.tile.TileAttunementCrafter",
+        "thelm.packagedastral.tile.TileConstellationCrafter",
+        "thelm.packagedastral.tile.TileTraitCrafter",
+        "thelm.packageddraconic.tile.TileFusionCrafter",
+        "thelm.packagedthaumic.tile.TileArcaneCrafter",
+        "thelm.packagedthaumic.tile.TileVirialArcaneCrafter",
+        "thelm.packagedthaumic.tile.TileCrucibleCrafter",
+        "thelm.packagedthaumic.tile.TileInfusionCrafter"
+}, remap = false)
+public abstract class MixinTileUpdate {
+
+    private static final Map<TileBase, Double> PackagedAcceleration$extraTickProgress = new WeakHashMap<>();
+    private static final ThreadLocal<Boolean> PackagedAcceleration$runningExtraTicks = ThreadLocal.withInitial(()->false);
+
+    @Inject(method = "update", at = @At("RETURN"), remap = false)
+    private void PackagedAcceleration$runExtraUpdates(CallbackInfo ci) {
+        if(PackagedAcceleration$runningExtraTicks.get()) {
+            return;
+        }
+        TileBase tile = (TileBase)(Object)this;
+        if(tile.getWorld() == null || tile.getWorld().isRemote || tile.isInvalid()) {
+            return;
+        }
+        int extraTicks = PackagedAcceleration$getExtraTicks(tile);
+        if(extraTicks <= 0) {
+            return;
+        }
+        PackagedAcceleration$runningExtraTicks.set(true);
+        try {
+            ITickable tickable = (ITickable) this;
+            for(int i = 0; i < extraTicks && !tile.isInvalid(); ++i) {
+                tickable.update();
+            }
+        }
+        finally {
+            PackagedAcceleration$runningExtraTicks.set(false);
+        }
+    }
+
+    private int PackagedAcceleration$getExtraTicks(TileBase tile) {
+        if(!SpeedCardHelper.isSupported(tile)) {
+            PackagedAcceleration$extraTickProgress.remove(tile);
+            return 0;
+        }
+        int cards = SpeedCardHelper.getCards(tile);
+        if(cards <= 0) {
+            PackagedAcceleration$extraTickProgress.remove(tile);
+            return 0;
+        }
+        double progress = PackagedAcceleration$extraTickProgress.getOrDefault(tile, 0D)+SpeedCardHelper.getMultiplier(cards)-1D;
+        int ticks = (int)progress;
+        progress -= ticks;
+        if(progress > 0D) {
+            PackagedAcceleration$extraTickProgress.put(tile, progress);
+        }
+        else {
+            PackagedAcceleration$extraTickProgress.remove(tile);
+        }
+        return ticks;
+    }
+}
